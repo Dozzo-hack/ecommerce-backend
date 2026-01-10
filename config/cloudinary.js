c2da0dbc6d5f002362309658c5aedd3d91092048
Dozzo-hack <wendjikengouapiduhamelange@gmail.com>
2026-01-10 17:00:14 +0100

    Add files via upload

---
+++ config/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: "dkdmtvhcx",
+  api_key: "171485965724523",
+  api_secret: "RXp7MZdUv2OY7FgEQtn1TYxQcqM",
+});
+
+module.exports = cloudinary;
